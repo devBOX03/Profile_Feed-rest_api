496767423930957adcb79dcd1438039371e5af2c
devBOX03 <debasish_padhi@outlook.com>
2018-04-27 11:51:33 +0530

    Token based authentification has been implemented.

---
+++ src/profile_feed/profiles_api/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+
+from . import models
+
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """A serializer for our user profile objects."""
+
+    class Meta:
+        model = models.UserProfile
+        fields = ('id', 'email', 'name', 'user_role','password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        """Create and return a new user."""
+
+        user = models.UserProfile(
+            email=validated_data['email'],
+            name=validated_data['name'],
+            user_role=validated_data['user_role']
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
+
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """A serializer for profile feed items."""
+
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id', 'user_profile', 'status_text', 'created_on')
+        extra_kwargs = {'user_profile': {'read_only': True}}
